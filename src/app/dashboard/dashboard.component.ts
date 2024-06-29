7870deccc1df7c093407ef970410754dfb749022
Juanse Franco <jsfc2199@gmail.com>
2024-06-29 15:55:14 -0500

    Aplicada estructura de directorios

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+})
+export class DashboardComponent {
+
+}
---
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-detection',
+  standalone: true,
+  imports: [],
+  templateUrl: './change-detection.component.html',
+})
+export class ChangeDetectionComponent {
+
+}
