abed4691a5bbdbde737149421e29dbfd4c7520a7
Juanse Franco <jsfc2199@gmail.com>
2024-06-29 17:03:06 -0500

    @for como nuevo control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -11,6 +11,9 @@ export default class ControlFlowComponent {
 
   public showContent = signal(false);
   public grade = signal<Grade>('A');
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'React', 'NestJs'])
+  public frameworks2 = signal([])
+
 
 
   public toggleContent(){
