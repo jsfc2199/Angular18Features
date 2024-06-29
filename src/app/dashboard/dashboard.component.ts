8e67f238e45a8c6cad410cf3b23a27909675b768
Juanse Franco <jsfc2199@gmail.com>
2024-06-29 16:27:16 -0500

    Side menu template usado en el dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from '../shared/side-menu/side-menu.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SideMenuComponent],
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
   imports: [],
   templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.css'
+  styleUrl: './side-menu.component.css',
 })
 export class SideMenuComponent {
-
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path) //escluimos los paths []
+    .filter((route) => !route.path?.includes(':')); //exluimos el path con :id
 }
