d4f541122f4856c44e8b6b87affafa2e362e043b
Juanse Franco <jsfc2199@gmail.com>
2024-06-29 17:14:01 -0500

    uso de Input como requerido obligatorio

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 type Grade = 'A'|'B'|'F'
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './control-flow.component.html',
 })
 export default class ControlFlowComponent {
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
-  templateUrl: './title.component.html',
-  styleUrl: './title.component.css'
+  template: ` <h1 class="text-3xl mb-5">{{ title }} - {{withShadow}}</h1> `,
 })
 export class TitleComponent {
+  //required inputs
+  @Input({ required: true }) title!: string;
 
+  //transformaciones en los inputs. Si viene se queda en true, sino es false
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
 }
