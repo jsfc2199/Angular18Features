454027a62441ce2b957ce5e68cd90145fa230389
Juanse Franco <jsfc2199@gmail.com>
2024-06-29 16:52:42 -0500

    @switch como nuevo control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 
+type Grade = 'A'|'B'|'F'
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -9,6 +10,8 @@ import { Component, signal } from '@angular/core';
 export default class ControlFlowComponent {
 
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
+
 
   public toggleContent(){
     this.showContent.update((value) => !value)
