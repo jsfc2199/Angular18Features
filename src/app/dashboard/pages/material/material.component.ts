32fe5d2eb12e1e1729e01ac5d13c9e12301e2dc0
Juanse Franco <jsfc2199@gmail.com>
2024-06-30 14:49:33 -0500

    mas uso de componentes de material

--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -4,6 +4,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import {
+  MatBottomSheet,
+  MatBottomSheetModule,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
+import { BottomSheetComponent } from './ui/bottom-sheet/bottom-sheet.component';
 
 @Component({
   selector: 'app-material',
@@ -14,8 +21,17 @@ import { CommonModule } from '@angular/common';
     MatIconModule,
     MatBadgeModule,
     MatButtonModule,
+    MatBottomSheetModule,
+    BottomSheetComponent,
+    MatListModule
   ],
   templateUrl: './material.component.html',
   styleUrl: './material.component.css',
 })
-export default class MaterialComponent {}
+export default class MaterialComponent {
+  constructor(private _bottomSheet: MatBottomSheet) {}
+
+  openBottomSheet(): void {
+    this._bottomSheet.open(BottomSheetComponent);
+  }
+}
