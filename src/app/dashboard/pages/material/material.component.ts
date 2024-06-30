afe1c6e4deb459718994a2aacd5565f10ff4226e
Juanse Franco <jsfc2199@gmail.com>
2024-06-30 14:39:02 -0500

    uso de componentes de material

--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatButtonModule,
+  ],
   templateUrl: './material.component.html',
-  styleUrl: './material.component.css'
+  styleUrl: './material.component.css',
 })
-export default class MaterialComponent {
-
-}
+export default class MaterialComponent {}
