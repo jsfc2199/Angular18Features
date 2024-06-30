974497e438e9b30f23d7bf5bbdaecda307a29e28
Juanse Franco <jsfc2199@gmail.com>
2024-06-30 12:35:27 -0500

    transiciones

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -9,9 +9,11 @@ import { TitleComponent } from '@shared/title/title.component';
     <app-title title="View Transition 1"></app-title>
 
     <section class="flex justify-start">
-      <img srcset="https://picsum.photos/id/237/200/300" alt="picsum" width="200" height="300">
+      <!-- en el style deben tener el mismo nombre de transiscion para poder apreciarlo y debe ser único entre imagenes -->
 
-      <div class="bg-blue-500 w-56 h-56"></div>
+      <img srcset="https://picsum.photos/id/237/200/300" alt="picsum" width="200" height="300" style="view-transition-name: dog1">
+
+      <div class="bg-blue-500 w-56 h-56" style="view-transition-name: box1"></div>
     </section>
   `,
 })
--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -8,10 +8,12 @@ import { TitleComponent } from '@shared/title/title.component';
   template: `
     <app-title title="View Transition 2"></app-title>
 
-    <section class="flex justify-start">
-      <img srcset="https://picsum.photos/id/237/200/300" alt="picsum" width="200" height="300">
+    <section class="flex justify-end">
+      <!-- en el style deben tener el mismo nombre de transiscion para poder apreciarlo y debe ser único entre imagenes -->
+      <img srcset="https://picsum.photos/id/237/200/300" alt="picsum" width="200" height="300"
+      style="view-transition-name: dog1">
 
-      <div class="bg-blue-500 w-56 h-56"></div>
+      <div class="bg-blue-800 w-20 h-20" style="view-transition-name: box1"></div>
     </section>
   `,
 })
