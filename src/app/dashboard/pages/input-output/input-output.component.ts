277d5ca902ce99568e3b566f486696a18a1da22f
Juanse Franco <jsfc2199@gmail.com>
2024-06-30 14:06:04 -0500

    añadiendo productos dinamicamente con interval para hacer el ejercicio de las funciones input y output

--- src/app/dashboard/pages/input-output/input-output.component.ts
+++ src/app/dashboard/pages/input-output/input-output.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductCardComponent } from './ui/product-card/product-card.component';
+import { Product } from '../../../interfaces/product';
+import { interval, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-input-output',
@@ -9,4 +11,35 @@ import { ProductCardComponent } from './ui/product-card/product-card.component';
 })
 export default class InputOutputComponent {
 
+  public products = signal<Product[]>([
+    {
+      id:1,
+      name: 'producto 1',
+      quantity: 0
+    },
+    {
+      id:2,
+      name: 'producto 2',
+      quantity: 0
+    }
+  ])
+
+  private internalSubscription = interval(1000).pipe(
+    tap(() => {
+      this.products.update((products) => [
+        ...products,
+        {
+          id: products.length +1,
+          name: ` product ${products.length +1 }`,
+          quantity: 0
+        }
+      ])
+    }),
+    take(7)
+  ).subscribe()
+
+  ngOnDestroy(): void {
+    this.internalSubscription.unsubscribe()
+
+  }
 }
