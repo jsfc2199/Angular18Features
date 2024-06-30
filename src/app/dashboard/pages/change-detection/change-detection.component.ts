171974a4e5bb44f2fd195a179e1702a88a6f82fe
Juanse Franco <jsfc2199@gmail.com>
2024-06-30 11:30:08 -0500

    Change detection usando on push para los cambios en señales y mejorar performance

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush, //por defecto es default y esto es lo nuevo que veremos, está menos atento a tantos cambios, y enforcar a señales
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title [title]='currentFrame()'></app-title>
+
+    <pre>{{ frameworksAsSignal() | json }}</pre>
+    <pre>{{ frameworksAsProperty | json }}</pre>
+  `,
 })
 export default class ChangeDetectionComponent {
 
+  public currentFrame = computed(()=> `change detection - ${this.frameworksAsSignal().name}`)
+  public frameworksAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworksAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    //forma tradicional en la que funciona angular respecto a los cambios
+    //si activamos el onPush del change detection, esto se dispara pero no hace ningún cambio
+    console.log('hecho')
+    setTimeout(() => {
+      // this.frameworksAsProperty.name = 'React';
+
+      //el beneficio es que todo se enfoca a señales y mejora el rendimiento
+      this.frameworksAsSignal.update((value) => ({
+        ...value,
+        name: 'react'
+      }))
+    }, 3000);
+  }
 }
